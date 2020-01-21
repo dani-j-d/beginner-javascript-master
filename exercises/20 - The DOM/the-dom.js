0f9a98d31c21bf2ee0614280f15e47754c38c5e7
dani-j-d <59181296+dani-j-d@users.noreply.github.com>
2020-01-21 16:35:12 -0600

    Coursework progress
    Completed several exercises

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,11 +1,24 @@
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const heading = document.querySelector('h2');
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const heading = document.querySelector('h2');
 
 
-console.log(p);
-console.log(imgs);
-//set text content property on that element
-heading.textContent = 'Wes is cool';
-console.log(heading.textContent);
-console.log(heading.innerText);
+// console.log(p);
+// console.log(imgs);
+// //set text content property on that element
+// heading.textContent = 'Wes is cool';
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+ 
+console.log(pic.classList);
+
+function toggleRound(){
+    pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
+pic.alt = 'Cute Pup';
+pic.width = 300;
